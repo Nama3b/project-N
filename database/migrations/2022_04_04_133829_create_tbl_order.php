28bd6ac516e4d91adf3541f210a5666f8ec4a868
Lee Thanh Long <76930578+Nama3b@users.noreply.github.com>
2022-04-14 23:27:59 +0700

    Add files via upload

---
+++ database/migrations/2022_04_04_133829_create_tbl_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_order', function (Blueprint $table) {
+            $table->Increments('order_id');
+            $table->Integer('client_id');
+            $table->Integer('checkClientInfo_id');
+            $table->Integer('payment_id');
+            $table->float('total');
+            $table->Integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_order');
+    }
+}
